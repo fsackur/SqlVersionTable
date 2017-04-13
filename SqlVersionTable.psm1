5e4ead436d58d70a3b2ae3dea59b8afe91297dee
fsackur <freddie.sackur@hotmail.com>
2017-04-13 22:57:45 +0100

    Improved UpdateType pattern matching

--- SqlVersionTable.psm1
+++ SqlVersionTable.psm1
@@ -1,15 +1,55 @@
 ﻿
 function Get-SqlVersionTable {
+    <#
+        .Synopsis
+        Returns list of SQL Server versions
+
+        .Description
+        Queries the excellent internet resource sqlserverbuilds.blogspot.com for the close-to-exhaustive list of public SQL Server builds.
+
+        Returned objects include versions, descriptions and links to the relevant BK articles.
+
+        .Parameter Version
+        Filters output by SQL release, e.g. 2012, 2008 R2. Default: 2008 R2 up to 2016.
+
+        .Parameter UpdateType
+        Filters output by update type, e.g. SP, RTM, CU.
+
+        .Example
+        PS C:\> Get-SqlVersionTable -Version '2008 R2', 2012, 2014 -UpdateType RTM, SP | ft Release, UpdateType, Build, 'KB / Description'
+
+        Release UpdateType Build      KB / Description                                  
+        ------- ---------- -----      ----------------                                  
+        2014    SP         12.0.5000  SQL Server 2014 Service Pack 2 (SP2)  Latest SP   
+        2014    SP         12.0.4100  SQL Server 2014 Service Pack 1 (SP1)              
+        2014    SP         12.0.4050  SQL Server 2014 Service Pack 1 (SP1) [withdrawn]  
+        2014    RTM        12.0.2000  SQL Server 2014 RTM  RTM                          
+        2012    SP         11.0.6020  SQL Server 2012 Service Pack 3 (SP3)  Latest SP   
+        2012    SP         11.0.5058  SQL Server 2012 Service Pack 2 (SP2)              
+        2012    SP         11.0.3000  SQL Server 2012 Service Pack 1 (SP1)              
+        2012    RTM        11.0.2100  SQL Server 2012 RTM  RTM                          
+        2008 R2 SP         10.50.6000 SQL Server 2008 R2 Service Pack 3 (SP3)  Latest SP
+        2008 R2 SP         10.50.4000 SQL Server 2008 R2 Service Pack 2 (SP2)           
+        2008 R2 SP         10.50.2500 SQL Server 2008 R2 Service Pack 1 (SP1)           
+        2008 R2 RTM        10.50.1600 SQL Server 2008 R2 RTM  RTM                       
+
+        Returns the build numbers of all RTM and SP releases of SQL Server 2008 R2 up to 2014
+    #>
     param(
         [Parameter(Position=0)]
         [Alias('Release')]
         [ValidateSet('v.Next', '2016', '2014', '2012', '2008 R2', '2008', '2005', '2000', '7.0')]
-        [string[]]$Version = ('2016', '2014', '2012', '2008 R2')
+        [string[]]$Version = ('2016', '2014', '2012', '2008 R2'),
+
+        [Parameter(Position=1)]
+        [ValidateSet('CTP', 'RC', 'RTM', 'GDR', 'Hotfix', 'CU', 'SP', 'Update')]
+        [string[]]$UpdateType
+                
     )
     
-    $Url = 'https://sqlserverbuilds.blogspot.co.uk/'
+    $Url = 'https://sqlserverbuilds.blogspot.com/'
     try {
-        $Blog = Invoke-WebRequest $Url -ErrorAction Stop
+        #$Blog = Invoke-WebRequest $Url -ErrorAction Stop
     } catch {
         Write-Error ([string]::Format(
             "Unable to fetch version table from {0}. {1}: {2}",
@@ -64,19 +104,20 @@ function Get-SqlVersionTable {
 
             $RowObj | Add-Member NoteProperty -Name UpdateType -Value $(
                 switch -Regex ($RowObj.'KB / Description') {
+                    'Hotfix|QFE' {'Hotfix'; break}
                     'Community Technology Preview' {'CTP'; break}
                     'Release Candidate' {'RC'; break}
-                    'RTM' {'RTM'; break}
+                    'RTM .* RTM' {'RTM'; break}
                     'GDR' {'GDR'; break}
-                    'Hotfix' {'Hotfix'; break}
-                    'Cumulative update' {'CU'; break}
-                    'Service Pack' {'SP'; break}
+                    'Cumulative update package \d+ \(CU\d+\)' {'CU'; break}
+                    '^(?!\d+ FIX).*Service Pack \d \(SP\d\)' {'SP'; break}
                     default {'Update'}
                 }
             )
 
-
-            $Output.Add($RowObj)
+            if (-not $PSBoundParameters.ContainsKey('UpdateType') -or ($UpdateType -contains $RowObj.UpdateType)) {
+                $Output.Add($RowObj)
+            }
         }
     }
 
